99cd21c8c7c9ba0cd0f4ae8714226b4be89b692b
unknown <da4adindu@gmail.com>
2022-04-01 03:18:27 +0100

    even more bugs

--- logic.php
+++ logic.php
@@ -17,6 +17,6 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
 
 // Destroy if not possible to create a connection
 if(!$conn){
-  echo "<h3 class='container bg-dark p-3 text-center text-warning rounded-lg mt-5'>Not able to establish Database Connection<h3>";
+  echo "<h2>Not able to establish Database Connection<h2>";
 }
 
