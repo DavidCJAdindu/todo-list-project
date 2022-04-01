0af8f9792675a846d5573273ca25bc35ab283dcf
unknown <da4adindu@gmail.com>
2022-04-01 02:42:20 +0100

    database connected

--- logic.php
+++ logic.php
@@ -1,20 +1,20 @@
  
 <?php 
 
-$sName = "localhost";
-$uName = "adindu-1";
-$pass = "adinduxvi";
-$db_name = "todo-list";
-
-try {
-    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
-                    $uName, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-  echo "Connection failed : ". $e->getMessage();
-} 
 
 
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 
 
 
